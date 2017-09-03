197e5a87471f88f64d08fb2ad4c219835d5db76d
yogendrasinghmehra <yogendra.ms95@gmail.com>
2017-09-03 18:06:55 +0530

    changes for likes added

--- blogs/App_Start/BundleConfig.cs
+++ blogs/App_Start/BundleConfig.cs
@@ -23,11 +23,11 @@ namespace blogs
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/admin/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
             //new theme css bundle
-            bundles.Add(new StyleBundle("~/Content/theme/css").Include(
+            bundles.Add(new StyleBundle("~/designe/theme/css").Include(
                      "~/Content/theme/css/plugin.css",
                      "~/Content/theme/css/style.css"));
 
